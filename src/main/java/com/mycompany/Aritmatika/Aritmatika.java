65bf4c8788868b8c0fe6f30da5b98344a8eb3fba
Gieryy <gierytian@gmail.com>
2022-12-16 08:32:42 +0700

    inisiasi project Aritmatika

---
+++ src/main/java/com/mycompany/Aritmatika/Aritmatika.java
@@ -0,0 +1,47 @@
+package com.mycompany.Aritmatika;
+
+public class Aritmatika {
+    private double bilsatu;
+    private double bildua;
+
+    public Aritmatika(double bilsatu, double bildua) {
+        this.bilsatu = bilsatu;
+        this.bildua = bildua;
+    }
+
+    public double getBilsatu() {
+        return bilsatu;
+    }
+
+    public void setBilsatu(double bilsatu) {
+        this.bilsatu = bilsatu;
+    }
+
+    public double getBildua() {
+        return bildua;
+    }
+
+    public void setBildua(double bildua) {
+        this.bildua = bildua;
+    }
+    
+   /* penjumlahan */
+    public double penjumlahan(){
+        return 0 ;
+    }
+    
+   /* pengurangan */
+    public double pengurangan(){
+        return 0;
+    }
+    
+   /* perkalian */
+    public double perkalian(){
+        return 0;
+    }
+    
+   /* pembagian */
+   public double pembagian(){
+       return 0;
+   }
+}
---
+++ src/main/java/com/mycompany/Aritmatika/BangunDatar.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Aritmatika;
+
+/**
+ *
+ * @author DELL LATITUDE 7480
+ */
+public class BangunDatar {
+    
+}
